81e18b22762de9d040c30a873ebec64843c5d54b
Алексей Сырцев <79574158+AlexSyrtsev@users.noreply.github.com>
2021-03-03 13:28:24 +0300

    AlexeySyrtsev.github.io
    Spa Template v.2

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$('.container-aboutus').slick({
+    dots: true,
+    infinite: true,
+    // autoplay: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+});
+
+$('.container-testimonial').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // speed: 1000,
+    dots: true,
+    
+    // prevArrow: $('.prev'),
+    // nextArrow: $('.next'),
+});
+
+// $('.container-aboutus').slick-prev({
+//     left: 100px;
+//     background: red; /*to notice it, is white*/
+// });
+// $('.container-aboutus').slick-next({
+//     right: 100px;
+//     background: red; /*to notice it, is white*/
+// });
